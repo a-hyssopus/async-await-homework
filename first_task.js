b3292620f353c4a0e55eb77c30a64a27c93a8e03
dasha <misery.hatred.anger@gmail.com>
2021-04-14 10:22:42 +0300

    Added a simplified solution for the 1st task

--- first_task.js
+++ first_task.js
@@ -1,12 +1,8 @@
 function delay(ms) {
-    async function innerFunc() {
-        let promise = new Promise(((resolve, reject) => {
-            setTimeout(() => resolve('The promise was resolved'), ms);
-            setTimeout(() => reject(new Error('Something went wrong')), ms)
-        }))
-        return await promise;
-    }
-    return innerFunc();
+    return new Promise ((resolve, reject) => {
+        setTimeout(() => resolve('The promise was resolved'), ms);
+        setTimeout(() => reject('The promise was not resolved'), ms);
+    })
 }
 
 delay(3000).then(() => alert('runs after indicted time'));
